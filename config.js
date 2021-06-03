0b02f0311f996200d3b5baa517f339590af0a64a
phamduythanh15 <85273283+phamduythanh15@users.noreply.github.com>
2021-06-03 18:12:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là câu hỏi`,
+    Đằng sau đây là câu hỏi`,
     btnIntro: 'next',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Thành đẹp trai đúng không ',
